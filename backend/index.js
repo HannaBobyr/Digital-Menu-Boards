d39f46ec77aa08d87018d3ccadfaab12c5c927f0
Hanna Bobyr <bobyranya3002@gmail.com>
2023-06-20 11:13:30 +0200

    add .gitignore

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,21 @@
 import express from "express";
+import jwt from "jsonwebtoken";
 
 const app = express();
+app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send("Welcome");
-})
+app.post("/auth/login", (req, res) => {
+  console.log(req.body);
+  res.send("Welcome user");
+});
 
-app.listen(4444, (err)=>{
-    if(err) {
-        console.log(err.message);
-    }
-    console.log("server listening on 4444");
-})
+app.get("/", (req, res) => {
+  res.send("Welcome");
+});
+
+app.listen(4444, (err) => {
+  if (err) {
+    console.log(err.message);
+  }
+  console.log("server listening on 4444");
+});
